98b1ea31e9bfd71a8541f2c09536aba058303112
sarathikannan13 <sarathitvm1@gmail.com>
2026-01-21 21:13:57 +0530

    llama commit

--- aura/llm_chat.py
+++ aura/llm_chat.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+from llama_cpp import Llama
 
 load_dotenv()
 
@@ -8,6 +9,8 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 SYSTEM_PROMPT = "You are AURA. Answer shortly and clearly in 2 lines."
+MODEL_PATH = "models/qwen2.5-0.5b.Q4_K_M.gguf"
+
 
 GEMINI_MODEL = "gemini-2.5-flash"
 GROQ_MODEL = "llama-3.3-70b-versatile"
@@ -58,22 +61,55 @@ def ask_online_llm(prompt):
         return ask_groq(prompt)
 
 
-def ask_llamacpp(prompt):
-    system_prompt = (
-        "You are AURA, a helpful robot assistant. "
-        "Answer briefly and conversationally (2–3 lines max)."
+# def ask_llamacpp(prompt):
+#     system_prompt = (
+#         "You are AURA, a helpful robot assistant. "
+#         "Answer briefly and conversationally (2–3 lines max)."
+#     )
+
+#     payload = {
+#         "prompt": f"{system_prompt}\n\nStudent: {prompt}\nAURA:",
+#         "n_predict": 60,
+#         "temperature": 0.3,
+#         "stop": ["\nStudent:", "\nAURA:"]
+#     }
+
+#     try:
+#         r = requests.post(LLAMACPP_URL, json=payload, timeout=60)
+#         r.raise_for_status()
+#         return r.json().get("content", "").strip()
+#     except Exception as e:
+#         return f"Offline model error: {e}"
+
+llm = Llama(
+    model_path=MODEL_PATH,
+    n_ctx=2048,
+    n_threads=4,     # set to CPU cores of your Raspberry Pi
+    n_batch=256,
+    verbose=False
+)
+
+# =======================
+# LLaMA.cpp (Offline)
+# =======================
+
+def ask_llamacpp(prompt: str) -> str:
+    full_prompt = (
+        f"{SYSTEM_PROMPT}\n\n"
+        f"Student: {prompt}\n"
+        f"AURA:"
     )
 
-    payload = {
-        "prompt": f"{system_prompt}\n\nStudent: {prompt}\nAURA:",
-        "n_predict": 60,
-        "temperature": 0.3,
-        "stop": ["\nStudent:", "\nAURA:"]
-    }
-
     try:
-        r = requests.post(LLAMACPP_URL, json=payload, timeout=60)
-        r.raise_for_status()
-        return r.json().get("content", "").strip()
+        output = llm(
+            full_prompt,
+            max_tokens=80,
+            temperature=0.3,
+            stop=["Student:", "AURA:"]
+        )
+
+        return output["choices"][0]["text"].strip()
+
     except Exception as e:
         return f"Offline model error: {e}"
+
